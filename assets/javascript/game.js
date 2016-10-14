d0682e76d6cc06d8a2045c651043f655b1ddccf0
justinbesteman <justin@besteman.io>
2016-10-14 00:32:18 -0400

    Finished javascript for the dashes and replacing dash as the guess are right

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,13 @@
-function dash(word) {
+function random() {
+
+	var selection = Math.floor((Math.random(selection) * 1));
+
+	return selection;
+
+}
+
+
+function dash(word , selection) {
 
 	var dash = [];
 
@@ -6,7 +15,52 @@ function dash(word) {
 			dash.push("_");	
 		}
 
+
 	return dash;
 
 }
 
+
+function split(word , selection) {
+
+	var splitword = [];
+
+	splitword = word[selection].split("");
+
+
+	return splitword;
+
+}
+
+
+function correct_letter(word , guess) {
+
+	var correct_letter = [];
+
+	for (var i = word.length - 1; i >= 0; i--) {
+			
+		if (word[i] == guess) {
+
+			correct_letter.push(i);
+
+		}
+
+	}	
+
+	return correct_letter;
+
+}
+
+
+function replace_dash(dash , right_i , guess) {
+
+
+	for (var i = right_i.length - 1; i >= 0; i--) {
+		
+		dash[right_i[i]] = guess;
+
+	}
+
+	return dash;
+
+}
