f45abe2ed517b95b4f1df044b506eacd98d6eb61
justinbesteman <justin@besteman.io>
2016-10-19 15:41:56 -0400

    Finished Project

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
 
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
- 
-
-var word_choice = ["courtney"]
-
-// Will return a random number
+ var word_choice = ["hi" , "hey" , "hello" , "bye" , "goodbye","surfer","stress","happy","shake", "i am not happy", "only you can prevent forest fire", "what is love", "dancing in the dark","i am just a small time girl", "dance monkey","i am nothing but a dream"];
+var won_counter = 0;
+ //Will return a random number
+var leng = word_choice.length;
+console.log(leng);
 function random_number() {
 
-	var selection = Math.floor((Math.random(selection) * 1));
+	var selection = Math.floor((Math.random(selection) * 16));
 
 	return selection;
 
@@ -81,7 +81,6 @@ function correct_letter(word , guess) {
 // Replaces the dashes with correct letters
 function replace_dash(dash , right_i , guess) {
 
-	for(var i = 0 ; i > right_i.length; i++)
 
 	for (var i = right_i.length - 1; i >= 0; i--) {
 		
@@ -93,55 +92,76 @@ function replace_dash(dash , right_i , guess) {
 
 }
 
-function lost(){
+function lost(split_word){
+	
 
-	var lost = "<p>You lose son</p>";
+	var lost = "<p>You lose son</p>"+ "<p>The Word was: </p> "+ split_word + "<br>"+ "<br>" +"<img src='assets/images/skull.png'>";
 
 	document.querySelector("#loss").innerHTML = lost;
 
-	play_again();
-}
+	$("#play_again").html("<p>Woud you like to play again</p>" + "<p>y or n</p>");
 
+	document.onkeyup = function(e) {
 
-function play_again() {
-
-	var play_again = "<p>Would you like to play again?</>" + "<p>If No type n</p>" + "<p>If Yes type y";
-
-	document.querySelector("#play_again").innerHTML = play_again;
-
-	var user_input = String.fromCharCode(event.keyCode).toLowerCase();
+	var user_input = String.fromCharCode(e.keyCode).toLowerCase();
 
 	if (user_input == "y" || user_input == "n") {
 
-		if (user_input == "y") {
+		if (user_input == "y") { 
 
-			reset();
-
-		} else {
+			main_game();
 
 		}
 
 	}
- 
-}
 
-function reset(){
-
-	var word = "";
-	var dashs = "";
-	var split_word = "";
-	var fails = 0;
-
+}	
 
 }
+
+function win(won_counter){
+
+
+	
+
+	$("#play_again").html("<p>Woud you like to play again</p>" + "<p>y or n</p>");
+	var won = "<p>You Won!!!!</p><br>" + won_counter;
+	document.querySelector("#loss").innerHTML = won;
+	$("#play_again").html("<p>Woud you like to play again</p>" + "<p>y or n</p>");
+
+	document.onkeyup = function(e) {
+
+		var user_input = String.fromCharCode(e.keyCode).toLowerCase();
+
+		if (user_input == "y" || user_input == "n") {
+
+			if (user_input == "y") { 
+
+				main_game();
+	
+			}
+
+		}
+
+	}	
+}
+
+
 
 function main_game() {
+
+	$("#play_again").html("");
+	$("#wrongs").html("");
+	$("#loss").html("");
 
 	var word = select_word(word_choice);
 	var dashs = dash(word);
 	var split_word = split(word);
 	var fails = 0;
 	var wrongarray = [];
+	var guess_Left = 5;
+	alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
 
 	var gameboard = "<p>Play Time</p>" + "<p>Game Board</p>" + dashs.join(" ");
 
@@ -151,9 +171,6 @@ function main_game() {
 
 
 	document.onkeyup = function(event) {
-
-		if(fails != 5){
-
 
 		var user_guess = String.fromCharCode(event.keyCode).toLowerCase();
 
@@ -170,17 +187,28 @@ function main_game() {
 	 			var index = alphabet.indexOf(user_guess);
 	 			alphabet.splice(index,1);
 	 			wrongarray.push(user_guess);
+
+	 			guess_Left--;
 	 			
+	 			$("#wrongs").append("Guesses left: " + guess_Left);
 
 	 			fails++;
 
-	 			if(fails == 2){
+	 			if(fails == 5){
 
-	 				lost();
+	 				
 
+	 				lost(word);
+							/*word = select_word(word_choice);
+							dashs = dash(word);
+							split_word = split(word);
+							fails = 0;
+							wrongarray = [];
+							alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+*/
 	 			}
 
-	 			var wrongs = "<p> Wrong Letters</p>" + wrongarray.join(" ");
+	 			var wrongs = "<p> Wrong Letters</p>" + wrongarray.join(" ") + "<p>Guesses left: </p>" + guess_Left;
 
 	 			document.querySelector('#wrongs').innerHTML = wrongs;
 
@@ -190,11 +218,27 @@ function main_game() {
 
 	 		document.querySelector('#game').innerHTML = gameboard;
 
-	 	}
+	 		if(dashs.indexOf("_") == -1 ){
 
-	 }
+	 				var won = "<p>You Won!!!!</p><br>" + won_counter;
 
-	}
+					document.querySelector("#loss").innerHTML = won;
 
-}
 
+							/*word = select_word(word_choice);
+							dashs = dash(word);
+							split_word = split(word);
+							fails = 0;
+							wrongarray = [];
+							alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];*/
+							won_counter++;
+							win(won_counter);
+						
+	
+	 			}
+
+			}
+		
+		}
+
+	}	
